bb7a565c941a7317fbb18bbe1b2e598110219caa
Jürgen Wandschneider <juergen@wandschneider.dev>
2024-01-17 13:28:29 +0100

    TG-192 Charakterliste
    Added method to get race name from enum

--- app/src/main/java/app/bambushain/finalfantasy/characters/CharacterViewModel.java
+++ app/src/main/java/app/bambushain/finalfantasy/characters/CharacterViewModel.java
@@ -1,13 +1,18 @@
 package app.bambushain.finalfantasy.characters;
 
+import android.content.Context;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+import app.bambushain.R;
 import app.bambushain.models.finalfantasy.CharacterRace;
 import app.bambushain.models.finalfantasy.FreeCompany;
 import dagger.hilt.android.lifecycle.HiltViewModel;
+import dagger.hilt.android.qualifiers.ApplicationContext;
 
 import javax.inject.Inject;
 import java.util.List;
+
+import static android.provider.Settings.System.getString;
 
 @HiltViewModel
 public class CharacterViewModel extends ViewModel {
@@ -20,5 +25,32 @@ public class CharacterViewModel extends ViewModel {
 
     @Inject
     public CharacterViewModel(){
+    }
+
+    @Inject
+    @ApplicationContext
+    Context context;
+
+    public String getRaceText(CharacterRace race) {
+        switch (race) {
+            case HYUR:
+                return context.getString(R.string.character_race_hyur);
+            case ELEZEN:
+                return context.getString(R.string.character_race_elezen);
+            case LALAFELL:
+                return context.getString(R.string.character_race_lalafell);
+            case MIQOTE:
+                return context.getString(R.string.character_race_minqote);
+            case ROEGADYN:
+                return context.getString(R.string.character_race_roegadyn);
+            case AURA:
+                return context.getString(R.string.character_race_aura);
+            case HROTHGAR:
+                return context.getString(R.string.character_race_hrothgar);
+            case VIERA:
+                return context.getString(R.string.character_race_viera);
+            default:
+                return "";
+        }
     }
 }
