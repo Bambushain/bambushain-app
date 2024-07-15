1146e137df70bb1b5dcb2056d73169cdfb064e86
Jürgen Wandschneider <juergen@bambushain.app>
2024-07-15 15:31:20 +0200

    TG-269 Login
    Started with Bottom Sheet menus

--- app/src/main/java/app/bambushain/composables/user/UserList.kt
+++ app/src/main/java/app/bambushain/composables/user/UserList.kt
@@ -1,18 +1,33 @@
 package app.bambushain.composables.user
 
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import app.bambushain.R
 import app.bambushain.api.apis.UserApi
 import app.bambushain.api.models.User
 import org.koin.compose.koinInject
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun UserList(navController: NavController, userApi: UserApi = koinInject()) {
     var userListState by remember { mutableStateOf<List<User>>(listOf()) }
+    var showBottomSheet by remember { mutableStateOf(false) }
+    val sheetState = rememberModalBottomSheetState(
+        skipPartiallyExpanded = false,
+    )
+
+    val snackbarHostState = remember { SnackbarHostState() }
 
     LaunchedEffect(userListState) {
         val response = userApi.getUsers()
@@ -21,9 +36,44 @@ fun UserList(navController: NavController, userApi: UserApi = koinInject()) {
         }
     }
 
-    LazyColumn(modifier = Modifier.fillMaxSize()) {
-        items(userListState) { user ->
-            UserCard(user, userApi)
+    Scaffold(
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(stringResource(R.string.user_list_top_bar))
+                },
+                actions = {
+                    IconButton(onClick = { showBottomSheet = true }) {
+                        Icon(
+                            imageVector = Icons.Default.Add,
+                            contentDescription = stringResource(R.string.user_list_add_user),
+                        )
+                    }
+                },
+                scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(),
+            )
+        }
+    ) { contentPadding ->
+        Surface(modifier = Modifier.padding(contentPadding)) {
+            LazyColumn(modifier = Modifier.fillMaxSize()) {
+                items(userListState) { user ->
+                    UserCard(user, userApi)
+                }
+            }
+            if (showBottomSheet) {
+                ModalBottomSheet(
+                    modifier = Modifier.fillMaxHeight(),
+                    sheetState = sheetState,
+                    onDismissRequest = { showBottomSheet = false }
+                ) {
+                    Text(
+                        "Test",
+                        modifier = Modifier.padding(16.dp)
+                    )
+                }
+            }
         }
     }
+
 }
